07cd3ee53d67e3cae4d558650ef4f02a7fe34cd4
Matt Vogel <matt@portlandwebdev.com>
2019-09-12 16:28:38 -0700

    fix blockchain client

--- lib/blockchain_client/yada.rb
+++ lib/blockchain_client/yada.rb
@@ -1,82 +1,104 @@
+# encoding: UTF-8
+# frozen_string_literal: true
+
 module BlockchainClient
-    class Yada < Base
-  
-      DEFAULT_FEATURES = {case_sensitive: true, cash_addr_format: false}.freeze
-  
-      def initialize(*)
-        super
-        @features = DEFAULT_FEATURES
-        @settings = {}
-      end
-  
-      def configure(settings = {})
-        # Clean client state during configure.
-        @client = nil
-        @settings.merge!(settings.slice(*SUPPORTED_SETTINGS))
-      end
-  
-      def fetch_block!(block_number)
-  
-        client.rest_call_get('/get-block?index=' + block_number)
-          .fetch('transactions').each_with_object([]) do |tx, txs_array|
-            txs = build_transaction(tx).map do |ntx|
-              Peatio::Transaction.new(ntx.merge(block_number: block_number))
-            end
-            txs_array.append(*txs)
-          end.yield_self { |txs_array| Peatio::Block.new(block_number, txs_array) }
-      rescue Yada::Client::Error => e
-        raise Peatio::Blockchain::ClientError, e
-      end
-  
-      def latest_block_number
-        client.rest_call_get('/get-height')
-      rescue Yada::Client::Error => e
-        raise Peatio::Blockchain::ClientError, e
-      end
-  
-      def load_balance_of_address!(address, _currency_id)
-        address_with_balance = client.rest_call_get('/explorer-get-balance?address=' + address)
-  
-        if address_with_balance.blank?
-          raise Peatio::Blockchain::UnavailableAddressBalanceError, address
-        end
-  
-        address_with_balance[1].to_d
-      rescue Yada::Client::Error => e
-        raise Peatio::Blockchain::ClientError, e
-      end
-  
-      private
-  
-      def build_transaction(tx_hash)
-        tx_hash.fetch('outputs')
-          .select do |entry|
-            entry.fetch('value').to_d > 0
-          end
-          .each_with_object([]) do |entry, formatted_txs|
-            no_currency_tx =
-              { 
-                hash: tx_hash['hash'], 
-                txout: 0,
-                to_address: entry['to'],
-                amount: entry.fetch('value').to_d,
-                status: 'success',
-                block_number: 0
-              }
-  
-              # Build transaction for each currency belonging to blockchain.
-              settings_fetch(:currencies).pluck(:id).each do |currency_id|
-                formatted_txs << no_currency_tx.merge(currency_id: currency_id)
-              end
-          end
-      end
-  
-      def client
-        @client ||= Yada::Client.new(settings_fetch(:server))
-      end
-  
-      def settings_fetch(key)
-        @settings.fetch(key) { raise Peatio::Blockchain::MissingSettingError, key.to_s }
+  class Yada < Base
+    def initialize(*)
+      super
+      @json_rpc_call_id  = 0
+      @json_rpc_endpoint = URI.parse(blockchain.server)
+    end
+
+    def endpoint
+      @json_rpc_endpoint
+    end
+
+    def load_balance!(address, currency)
+      json_rpc(:listunspent, [1, 10_000_000, [address]])
+        .fetch('result')
+        .sum { |vout| vout['amount'] }
+    end
+
+    def load_deposit!(txid)
+      json_rpc(:gettransaction, [normalize_txid(txid)]).fetch('result').yield_self { |tx| build_standalone_deposit(tx) }
+    end
+
+    def create_address!(options = {})
+      { address: normalize_address(json_rpc(:getnewaddress).fetch('result')) }
+    end
+
+    def create_withdrawal!(issuer, recipient, amount, options = {})
+      json_rpc(:settxfee, [options[:fee]]) if options.key?(:fee)
+      json_rpc(:sendtoaddress, [normalize_address(recipient.fetch(:address)), amount])
+        .fetch('result')
+        .yield_self(&method(:normalize_txid))
+    end
+
+    def latest_block_number
+      Rails.cache.fetch "latest_#{self.class.name.underscore}_block_number", expires_in: 5.seconds do
+        json_rpc(:getblockcount).fetch('result')
       end
     end
+
+    def get_block(block_hash)
+      json_rpc(:getblock, [block_hash, 2]).fetch('result')
+    end
+
+    def get_block_hash(height)
+      current_block   = height || 0
+      json_rpc(:getblockhash, [current_block]).fetch('result')
+    end
+
+    def to_address(tx)
+      tx.fetch('vout').map{|v| normalize_address(v['scriptPubKey']['addresses'][0]) if v['scriptPubKey'].has_key?('addresses')}.compact
+    end
+
+    def build_transaction(tx, current_block, address)
+      entries = tx.fetch('vout').map do |item|
+
+        next if item.fetch('value').to_d <= 0
+        next unless item['scriptPubKey'].has_key?('addresses')
+        next if address != normalize_address(item['scriptPubKey']['addresses'][0])
+
+        { amount:   item.fetch('value').to_d,
+          address:  normalize_address(item['scriptPubKey']['addresses'][0]),
+          txout:    item.fetch('n') }
+      end.compact
+
+      { id:            normalize_txid(tx.fetch('txid')),
+        block_number:  current_block,
+        entries:       entries }
+    end
+
+    def get_unconfirmed_txns
+      json_rpc(:getrawmempool).fetch('result').map(&method(:get_raw_transaction))
+    end
+
+    def get_raw_transaction(txid)
+      json_rpc(:getrawtransaction, [txid, true]).fetch('result')
+    end
+
+  protected
+
+    def connection
+      Faraday.new(@json_rpc_endpoint).tap do |connection|
+        unless @json_rpc_endpoint.user.blank?
+          connection.basic_auth(@json_rpc_endpoint.user, @json_rpc_endpoint.password)
+        end
+      end
+    end
+    memoize :connection
+
+    def json_rpc(method, params = [])
+      response = connection.post \
+        '/',
+        { jsonrpc: '1.0', method: method, params: params }.to_json,
+        { 'Accept'       => 'application/json',
+          'Content-Type' => 'application/json' }
+      response.assert_success!
+      response = JSON.parse(response.body)
+      response['error'].tap { |error| raise Error, error.inspect if error }
+      response
+    end
   end
+end
