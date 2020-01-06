40f7b5e5df04caf8204aa2ec12c3a231e0c1e395
Matt Vogel <matt@portlandwebdev.com>
2020-01-05 22:02:53 -0800

    add api secret to wallet prop

--- lib/peatio/yada/wallet.rb
+++ lib/peatio/yada/wallet.rb
@@ -31,7 +31,7 @@ module Yada
     def create_transaction!(transaction, options = {})
       Rails.logger.info { @wallet }
       @wallet = @wallet.to_wallet_api_settings
-      .merg(uri: @wallet.fetch(:uri), address: @wallet.fetch(:address))
+      .merge(uri: @wallet.fetch(:uri), address: @wallet.fetch(:address))
       .compact
       client.unlock(@wallet.fetch(:secret))
       txn = client.rest_call_post(
